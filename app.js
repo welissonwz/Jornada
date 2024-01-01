7c674639cbdc0db73a54b66917b1b87f6dce58b0
Welisson Santos <Welisson_martinss@hotmail.com>
2024-01-01 10:29:31 -0300

    Jornada

---
+++ app.js
@@ -0,0 +1,11 @@
+alert("Seja Bem Vindo")
+let dia1 = 'Sabado';
+let dia2 = 'Domingo';
+let semana = prompt('Escolha um dia da semana?');
+
+//se a semana for igual dia1 ou dia2
+if(semana == dia1 || dia2){
+    alert('Bom final de semana!')
+} else {
+    alert('Boa semana!')
+}
