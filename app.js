6222d47962a5d39e3eae9cb2b14ffaf64e1b27ee
Welisson Santos <Welisson_martinss@hotmail.com>
2024-01-04 12:37:00 -0300

    feriado

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 alert("Seja Bem Vindo")
 let dia1 = 'Sabado';
 let dia2 = 'Domingo';
+let dia3 = "Feriado"
 let semana = prompt('Escolha um dia da semana?');
-
-//se a semana for igual dia1 ou dia2
-if(semana == dia1 || dia2){
+let Anonovo = dia3
+//se a semana for igual dia1 ou dia2 / se for feriado ficará bom feriado
+while(semana =! Feriado){
+    if(semana == dia1 || dia2){
     alert('Bom final de semana!')
 } else {
     alert('Boa semana!')
+    if(semana=dia3){
+       console.log('Bom Feriado')
+    }
+}
 }
